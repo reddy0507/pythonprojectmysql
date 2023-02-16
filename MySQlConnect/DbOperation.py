7ae88fadd13ac26d715483e6e4699acf6659e46f
reddy0507 <117848588+reddy0507@users.noreply.github.com>
2023-02-16 17:54:00 +0530

    Update DbOperation.py

--- MySQlConnect/DbOperation.py
+++ MySQlConnect/DbOperation.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 con = mysql.connector.connect(host="localhost", port=3306, user="root",passwd="root", database="mydb")
 curs = con.cursor()
-curs.execute("insert into student(id,SName) values(3,'reddy')")
+curs.execute("insert into student(id,SName) values(5,'reddy')")
 con.commit()
 con.close()
 print("Finished --------")
